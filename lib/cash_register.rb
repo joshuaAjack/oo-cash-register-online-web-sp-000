c5d5f7910e3feb26e8fa617170331cdc9e1b087b
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-03-09 03:11:17 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,9 +13,14 @@ class CashRegister
 def add_item
   self.price +=1
 end
-def apply_discount 
-  20.0.class
-end 
+ def apply_discount
+    if @discount == 0
+      "There is no discount to apply."
+    else
+      self.total -= (0.01 * @discount * @total).to_i
+      "After the discount, the total comes to $#{self.total}."
+    end
+  end
 def items 
 end 
 def void_last_transaction
