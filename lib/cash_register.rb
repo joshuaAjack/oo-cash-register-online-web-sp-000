c73e043a1a366042c502e0b116a547b38d5d0b2f
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-03-09 03:24:53 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,39 +1,39 @@
 class CashRegister
-  attr_accessor :total, :discount, :quantity, :price
+  attr_accessor :discount, :purchases, :total, :transaction
 
-  def initialize(discount = 0)
-    @total = 0
-    @discount = discount
-    @price = price
-    @quantity = quantity
-    @items = []
-    @transactions = []
+  def initialize(discount=nil)
+    self.total = 0
+    self.discount = discount if discount != nil
+    self.purchases = []
   end
 
-  def add_item(title, price, quantity = 1)
-    @total += price * quantity
-    @transactions << price
-    i = quantity
-    until i == 0 do
-      @items << title
-      i -= 1
+  def add_item(name, price, quantity=1)
+    self.transaction = [name, price, quantity]
+    quantity.times do
+      self.purchases.push self.transaction[0]
+      self.total += self.transaction[1]
     end
   end
 
   def apply_discount
-    if @discount == 0
-      "There is no discount to apply."
+    if discount
+      self.total = self.total - self.total*self.discount/100
+      result = "After the discount, the total comes to $#{self.total}."
     else
-      self.total -= (0.01 * @discount * @total).to_i
-      "After the discount, the total comes to $#{self.total}."
+      result = "There is no discount to apply."
     end
+    return result
   end
 
   def items
-    @items
+    return self.purchases
   end
 
   def void_last_transaction
-    self.total = @total - @transactions
+    quantity = self.transaction[2]
+    quantity.times do
+      self.purchases.pop
+      self.total -= self.transaction[1]
+    end
   end
 end
